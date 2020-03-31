d9f9af03a061b35f84bde65039fe0a207ac3e5aa
amirrezaask <raskarpour@gmail.com>
2020-03-31 22:44:15 +0430

    ibuffer, hl-indent

--- emacs-config.el
+++ emacs-config.el
@@ -4,7 +4,7 @@
     
     ; UI features
     :theme :spacemacs-dark
-    :font ("Jetbrains Mono" 11)
+    :font ("mononoki" 11)
     :doom-modeline
     ;; :spacemacs-modeline
     :icons
@@ -14,10 +14,11 @@
     :ivy
     ;; :helm
     ;; :ido
+    :ibuffer
     
     ; Editor
     ;; :multiple-cursor ; multiple edits
-    ;; :whitespace-mode ; highlight indents
+    :hl-indent
     ;; :which-key ; helps us when we only remember part of a keybinding
     ;; :neotree ; a side tree file browser like NERDtree for vim
     
@@ -37,30 +38,26 @@
     
     ; Supported languages
     :assembly           ; closest thing to metal
-    :cc                 ; C/C++/Obj-C madness
+    :c/c++              ; C/C++/Obj-C madness
     :clojure            ; lisp on jvm
-    ;;common-lisp       ; common things lisps have
-    ;;csharp            ; unity, .NET, and mono shenanigans
+    :csharp             ; unity, .NET, and mono shenanigans
     :dockerfile         ; VMs of new era
     :data               ; support all data formats that I know
     :emacs-lisp         ; emacs language
     :elixir             ; erlang done right
-    ;;erlang            ; pass the message
-    ;;fsharp            ; Haskell on .NET
+    :fsharp             ; Haskell on .NET
     :golang             ; New edition of C with garbage collector
     :haskell            ; a language thats lazier than I am
     :java               ; let me guess ? You are in a retirement home
     :javascript         ; akh :(
-    ;;kotlin            ; bearable java(javascript)
-    ;;latex             ; just use the org mode :)
     :lua                ; one-based indices? one-based indices
     :markdown           ; writing docs for people to ignore
     :org                ; Markdown done correctly
     :php                ; so it goes back to your childhood
     :python             ; simples better than complex
-    ;;racket            ; a DSL for DSLs
+    :racket             ; a DSL for DSLs
     :rust               ; new actuall C replacement
-    ;;scala             ; java + functional programming
+    :scala              ; java + functional programming
     :sh                 ; she sells {ba,z,fi}sh shells on the C xor
     :web                ; the tubes
     )
