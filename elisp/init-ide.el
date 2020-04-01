424e288fcdc24f139e064093f17c7e4a300f8af1
amirrezaask <raskarpour@gmail.com>
2020-04-01 04:51:16 +0430

    some renames

--- elisp/init-ide.el
+++ elisp/init-ide.el
@@ -3,34 +3,46 @@
 			 :hook
 			 (prog-mode . flycheck-mode))
 
-(if-feature-use-package! :dap
+(if-feature-use-package! :debugger-adapter-protocol
 			 dap-mode
 			 :hook
 			 prog-mode)
 
-(if-feature-use-package! :magit
+(if-feature-use-package! :git
 			 magit
 			 :commands (magit-status)
 			 :init
 			 (bindkey global-map 'magit-status :holy "C-x g" :evil (:normal "SPC g s")))
 
-(if-feature-use-package! :magit
+(if-feature-use-package! :git
 			 diff-hl
 			 :config
 			 (global-diff-hl-mode))
 
-(if-feature-use-package! :magit
+(if-feature-use-package! :git
 			 forge
 			 :init
 			 (add-hook 'magit-mode-hook (lambda () (require 'forge)))
 			 )
+
+(if-feature-use-package! :git
+			 gitconfig-mode
+			 :mode "/\\.gitconfig\\'")
+
+(if-feature-use-package! :git
+			 gitignore-mode
+			 :mode "/\\.gitignore\\'")
+
+(if-feature-use-package! :git
+			 gitattributes-mode
+			 :mode "/\\.gitattributes\\'")
 
 (if-feature-use-package! :evil
 			 evil-magit
 			 :init
 			 (add-hook 'magit-mode-hook (lambda () (require 'evil-magit))))
 
-(if-feature-use-package! :lsp
+(if-feature-use-package! :language-server-protocol
 			 lsp-mode
 			 :defer t
 			 :hook
@@ -38,7 +50,7 @@
 			 :config
 			 (bindkey lsp-mode-map 'treemacs :holy "C-x t" :evil (:normal "SPC l t")))
 
-(if-feature-use-package! :lsp
+(if-feature-use-package! :language-server-protocol
 			 lsp-treemacs
 			 :commands lsp-treemacs-errors-list)
 
@@ -52,17 +64,16 @@
 (if-feature-use-package! :snippets
 			 yasnippet-snippets)
 
-(if-feature-use-package! :lsp
+(if-feature-use-package! :language-server-protocol
 			 lsp-ui
 			 :commands lsp-ui-mode)
 
-(if-feature-use-package! :lsp
+(if-feature-use-package! :language-server-protocol
 			 company-lsp
 			 :commands (company-lsp)
-			 :hook (lsp-mode)
 			 )
 
-(if-feature-use-package! :company
+(if-feature-use-package! :auto-completion
 			 company
 			 :config
 			 (global-company-mode t)
