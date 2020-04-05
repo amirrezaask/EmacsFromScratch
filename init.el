7ec10a29f62998746ae0eea029e4318e1cc43ea5
amirrezaask <raskarpour@gmail.com>
2020-04-06 03:51:22 +0430

    literate style from scratch.

--- init.el
+++ init.el
@@ -1,12 +1,3 @@
-(setq debug-on-error t)
-(add-to-list 'load-path (expand-file-name "elisp" user-emacs-directory))
-(require 'core)
-(require-all (expand-file-name "elisp" user-emacs-directory))
-
-
-(load-theme 'doom-one t)
-(font! "Jetbrains Mono" 13)
-(message "startup took %s" (- (float-time) start-timestamp))
-(provide 'init)
-;;; init.el ends here
+(setq vc-follow-symlinks t)
+(org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
  
