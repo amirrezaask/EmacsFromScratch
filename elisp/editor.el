7cc68dbff68a1f712aa12aff96a9601e9ad40473
amirrezaask <raskarpour@gmail.com>
2020-04-06 01:45:49 +0430

    use newer display-line-numbers-mode

--- elisp/editor.el
+++ elisp/editor.el
@@ -74,7 +74,8 @@
 (setq-default cursor-type 'bar)
 (global-hl-line-mode 1)
 (blink-cursor-mode 0)
-(global-linum-mode 1)
+(display-line-numbers-mode 1)
+
 
 (setq custom-file "~/.__custom.el")
 (setq make-backup-files nil) ;; turn off emacs annoying ~ files
