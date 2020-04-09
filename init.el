089d02458f0678a68c612276ca1cc9647f44c083
amirrezaask <raskarpour@gmail.com>
2020-04-09 06:18:10 +0430

    add GNU notice to init

--- init.el
+++ init.el
@@ -1,2 +1,32 @@
-(load-file (expand-file-name "hans/bootstrap.el" user-emacs-directory))
+;;; init.el --- init file                            -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  amirrezaask
+
+;; Author: amirrezaask <raskarpour@gmail.com>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(load-file (expand-file-name "elisp/bootstrap.el" user-emacs-directory))
+
+(provide 'init)
+;;; init.el ends here
+
 
