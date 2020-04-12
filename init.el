e5d846612e2b0d3163b10b8201a8b7ac268f0335
amirrezaask <raskarpour@gmail.com>
2020-04-12 05:41:38 +0430

    EP3

--- init.el
+++ init.el
@@ -19,7 +19,7 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
-;; (load-file "~/dev/emacs-from-scratch/command-log-mode.el")
+(load-file "~/dev/emacs-from-scratch/command-log-mode.el")
 
 (if (null package-archive-contents)
     (package-refresh-contents))
@@ -42,7 +42,7 @@
   :bind (("C-c C-c f" . counsel-fzf)
 	 ("C-c C-c s" . counsel-rg)))
 
-;; C-x => emacs built-in 
+;; C-x * => emacs built-in 
 ;; C-c C-* *** => major mode
 ;; C-c * => minor mode / user pref
 ;; fn => user pref
@@ -58,7 +58,7 @@
 (use-package lsp-mode
   :ensure t
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         (python-mode . lsp)
+         ((python-mode go-mode) . lsp)
          ;; if you want which-key integration
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
@@ -72,22 +72,31 @@
 (use-package magit :ensure t
   :bind (("C-x g" . magit)))
 
+(defun go-save-hooks ()
+  (interactive)
+  (add-hook 'before-save-hook 'lsp-organize-imports t t)
+  (add-hook 'before-save-hook 'lsp-format-buffer t t))
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("1526aeed166165811eefd9a6f9176061ec3d121ba39500af2048073bea80911e" default)))
- '(package-selected-packages
-   (quote
-    (which-key use-package sudo-edit spacemacs-theme smex rainbow-delimiters python-mode py-autopep8 pipenv phpunit php-mode modus-vivendi-theme modus-operandi-theme magit lsp-ui lsp-python-ms lsp-ivy kubel jinja2-mode iedit ido-vertical-mode ido-completing-read+ ibuffer-projectile hl-todo highlight-indent-guides gotest go-add-tags gitignore-mode gitconfig-mode gitattributes-mode git-messenger gcmh flycheck-rust expand-region exec-path-from-shell dumb-jump doom-themes doom-modeline dockerfile-mode docker diff-hl crux counsel-projectile company-prescient clj-refactor cargo badwolf-theme all-the-icons-dired ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package go-mode
+  :ensure t
+  :mode "\\.go\\'"
+  :config
+  (add-hook 'go-mode-hook 'go-save-hooks)
+  )
+
+(use-package go-add-tags
+  :ensure t
+  :config
+  (define-key go-mode-map (kbd "C-c C-g s t") 'go-add-tags))
+
+(use-package gotest
+  :ensure t
+  :config
+  (define-key go-mode-map (kbd "C-c C-g t t") 'go-test-current-test)
+  (define-key go-mode-map (kbd "C-c C-g t f") 'go-test-current-file))
+
+;; global-map
+;; go-mode-map
+;; python-mode-map
+
